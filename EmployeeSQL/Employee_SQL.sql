7dc873265905a40d653daf152186bf4b9817135c
Yve <yve.duncan@gmail.com>
2023-08-18 14:29:30 -0400

    adding the rest of my homework

--- EmployeeSQL/Employee_SQL.sql
+++ EmployeeSQL/Employee_SQL.sql
@@ -56,7 +56,33 @@ JOIN dept_manager ON department.department_no = dept_manager.dept_no
 JOIN employees ON dept_manager.emp_no = employees.emp_no; 
 
 -- List the department number for each employee along with that employee’s employee number, last name, first name, and department name.
-SELECT employees.emp_no, employees.last_name, employees.first_name, department.dept_name
+SELECT employees.emp_no, employees.last_name, employees.first_name, department.dept_name, department.department_no
 FROM employees
 JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
 JOIN department ON dept_emp.dept_no = department.department_no;
+
+-- List first name, last name, and sex of each employee whose first name is Hercules and whose last name begins with the letter B.
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+--List each employee in the Sales department, including their employee number, last name, and first name.
+SELECT employees.emp_no, employees.last_name, employees.first_name
+FROM employees
+JOIN dept_emp AS de ON employees.emp_no = de.emp_no
+JOIN department AS d ON de.dept_no = d.department_no
+WHERE d.dept_name = 'Sales';
+
+-- List each employee in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT employees.emp_no, employees.last_name, employees.first_name, department.dept_name
+FROM employees
+JOIN dept_emp ON employees.emp_no = dept_emp.emp_no
+JOIN department ON dept_emp.dept_no = department.department_no
+WHERE department.dept_name IN ('Sales', 'Development');
+
+-- List the frequency counts, in descending order, of all the employee last names (that is, how many employees share each last name).
+SELECT employees.last_name, COUNT (employees.last_name) AS frequency
+FROM employees
+GROUP BY employees.last_name
+ORDER BY frequency DESC;
+
